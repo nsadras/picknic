3e0adbb8bffe96b95ba8d63cfd89aa8acd3f1ad1
nitin <nitin.sadras@gmail.com>
2013-12-30 14:41:19 -0800

    added a master-detail-flow activity that is launched on login. UNTESTED

--- src/com/picknic/android/LoginActivity.java
+++ src/com/picknic/android/LoginActivity.java
@@ -6,6 +6,7 @@ import java.util.List;
 import android.app.Activity;
 import android.app.Dialog;
 import android.app.ProgressDialog;
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
@@ -51,18 +52,24 @@ public class LoginActivity extends Activity {
                         } else if (user.isNew()) {
                                 Log.d(PicknicApplication.TAG,
                                                 "User signed up and logged in through Facebook!");
-                                //go to next activity;
+                                showEventListActivity();
                         } else {
                                 Log.d(PicknicApplication.TAG,
                                                 "User logged in through Facebook!");
-                                //go to next activity;                              
+                                showEventListActivity();                              
                         }
                 }
         });
     }
 
-
-
+    /**
+     * Launch the EventListActivity
+     */
+    private void showEventListActivity(){
+    	Intent intent = new Intent(this,EventListActivity.class);
+    	startActivity(intent);
+    }
+    
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
